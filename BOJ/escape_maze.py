4e67fc37cd741a41e810ed7f41f86ab975c2e086
JUHONG PARK <corallines@naver.com>
2020-06-12 13:49:41 +0900

    add dfs in escape maze and if visited sentence

--- BOJ/escape_maze.py
+++ BOJ/escape_maze.py
@@ -16,16 +16,37 @@ def bfs(graph_list, root_i, root_j, M, N):
 
     while queue:
         [i, j] = queue.pop(0)
-        visited.append([i, j])
-        for k in range(4):
-            nx = i + dx[k]
-            ny = j + dy[k]
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph_list[nx][ny] == 1 :
-                    if not [nx, ny] in visited:
-                        queue.append([nx, ny])
-                        distance[nx][ny] = distance[i][j] + 1
+        if not [i, j] in visited:
+            visited.append([i, j])
+            for k in range(4):
+                nx = i + dx[k]
+                ny = j + dy[k]
+                if 0 <= nx < N and 0 <= ny < M:
+                    if graph_list[nx][ny] == 1 :
+                        if not [nx, ny] in visited:
+                            queue.append([nx, ny])
+                            distance[nx][ny] = distance[i][j] + 1
+    return distance[-1][-1]
 
-    return distance[N - 1][M - 1]
+def dfs(graph_list, root_i, root_j, M, N):
+    visited = []
+    stack = [[root_i, root_j]]
+    distance = [[0 for _ in range(M)] for _ in range(N)]
+    distance[root_i][root_j] = 1
+
+    while stack:
+        [i, j] = stack.pop()
+        if not [i, j] in visited:
+            visited.append([i, j])
+            for k in range(4):
+                nx = i + dx[k]
+                ny = j + dy[k]
+                if 0 <= nx < N and 0 <= ny < M:
+                    if graph_list[nx][ny] == 1:
+                        if not [nx, ny] in visited:
+                            stack.append([nx, ny])
+                            distance[nx][ny] = distance[i][j] + 1
+    return distance[-1][-1]
 
 print(bfs(graph_list, 0, 0, M, N))
+#print(dfs(graph_list, 0, 0, M, N))
