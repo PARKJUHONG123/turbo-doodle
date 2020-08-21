ec82a50f7b0eb6c56e7249ac30289e294b8810ab
PARKJUHONG123 <corallines@naver.com>
2020-08-21 21:36:07 +0900

    cannot solve poly

---
+++ ALGOSPOT/8_13_POLY.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 19:10:21 2020
+
+@author: elin9
+"""
+
+MOD = 10000000
+file = open("8_13_input.txt", "r")
+C = int(file.readline())
+
+def poly():
+    return 0
+
+for _ in range(C):
+    n = int(file.readline())
